4fdeae5f2170ebade5cd059c71f0bcdd6f5471f4
rathmann <rathmann>
2018-05-16 16:10:16 +0000

    unused include removed

--- qwt/playground/symbols/symbols.cpp
+++ qwt/playground/symbols/symbols.cpp
@@ -7,7 +7,6 @@
 #include <qwt_plot.h>
 #include <qwt_plot_marker.h>
 #include <qwt_plot_curve.h>
-#include <qwt_plot_grid.h>
 #include <qwt_symbol.h>
 #include <qwt_graphic.h>
 #include <qwt_legend.h>
