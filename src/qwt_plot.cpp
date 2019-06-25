d1043ee8aa6e70f217b72e3846a6cd81704adfed
rathmann <rathmann>
2019-06-25 08:30:34 +0000

    comment fixed

--- src/qwt_plot.cpp
+++ src/qwt_plot.cpp
@@ -26,7 +26,7 @@
 
 static inline void qwtEnableLegendItems( QwtPlot *plot, bool on )
 {
-    // gcc seems to have problems with const char sig[] in combination certain options
+    // gcc seems to have problems with const char sig[] in combination with certain options
     const char* sig = SIGNAL(legendDataChanged(QVariant,QList<QwtLegendData>));
     const char* slot = SLOT(updateLegendItems(QVariant,QList<QwtLegendData>));
 
